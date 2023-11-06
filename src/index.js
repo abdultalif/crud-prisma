583b7ad34ccd3cd859697829d9f9ec877c3e2216
abdultalif <abdultalif85@gmail.com>
2023-11-07 01:16:21 +0700

    ambil data

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import express from "express";
 import dotenv from "dotenv";
+import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient();
 const app = express();
 dotenv.config();
 const port = process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.send('test')
+app.get('/products', async (req, res) => {
+    const products = await prisma.product.findMany();
+    res.send(products);
 });
 
 app.listen(port, () => {
